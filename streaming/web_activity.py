ef2e2329998faadaa1620fbe96a268353773b717
Cyber MacGeddon <cybermaggedon@gmail.com>
2016-08-26 16:32:15 +0100

    Added BQ interface to web activity

--- streaming/web_activity.py
+++ streaming/web_activity.py
@@ -5,11 +5,24 @@ import zmq
 import socket
 import json
 
+############################################################################
+
+timeout = 60
+curation_time = 5
+
+############################################################################
+
+private = "private.json"
+project = None
+dataset = "cyberprobe"
+table = "activity"
+
+############################################################################
+
 fqdn = socket.getfqdn()
 ctxt = zmq.Context()
 skt = ctxt.socket(zmq.PULL)
 port = skt.bind_to_random_port("tcp://*")
-
 input="tcp://%s:%d" % (fqdn, port)
 
 print "INIT"
@@ -17,12 +30,127 @@ print "INPUT:input:%s" % input
 print "RUNNING"
 sys.stdout.flush()
 
+############################################################################
+
+# Creds
+scopes = ['https://www.googleapis.com/auth/bigquery']
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name(private,
+                                                               scopes=scopes)
+
+if project == None:
+    project = json.loads(open(private,'r').read())['project_id']
+    print project
+
+http = Http()
+http_auth = credentials.authorize(http)
+service = build('bigquery', 'v2', http=http_auth)
+tables = service.tables()
+tabledata = service.tabledata()
+jobs = service.jobs()
+
+############################################################################
+
+# Create table if not exists.
+
+try:
+    table_info = tables.get(projectId=project, datasetId=dataset,
+                            tableId=table).\
+                            execute(http)
+    found=True
+except googleapiclient.errors.HttpError:
+    found=False
+
+if found:
+    print "Table %s exists." % table
+else:
+
+    body = {
+        "tableReference": {
+            "projectId": project,
+            "datasetId": dataset,
+            "tableId": table
+        },
+        "kind": "bigquery#table",
+        "description": "fingerprint table",
+        "schema": {
+            "fields": [
+                {
+                    "name": "device",
+                    "mode": "REQUIRED",
+                    "type": "STRING"
+                },
+                {
+                    "name": "host",
+                    "mode": "REQUIRED",
+                    "type": "STRING"
+                },
+                {
+                    "name": "protocol",
+                    "mode": "REQUIRED",
+                    "type": "STRING"
+                },
+                {
+                    "name": "first",
+                    "mode": "REQUIRED",
+                    "type": "TIMESTAMP"
+                },
+                {
+                    "name": "last",
+                    "mode": "REQUIRED",
+                    "type": "TIMESTAMP"
+                }
+            ]
+        }
+    }
+
+    try: 
+        table_info = tables.insert(projectId=project, datasetId=dataset,
+                                   body=body).execute(http)
+        print "Table %s created." % table
+    except googleapiclient.errors.HttpError, e:
+        print "Table creation failed."
+        print e
+        sys.exit(0)
+
+############################################################################
+
 activity = {}
 
-timeout = 60
-curation_time = 5
+############################################################################
 
-last_curate=0
+def insert_activity(device, host, protocol, first, last):
+
+    # Store in BigQuery
+    obj = {
+        "device": device,
+        "host": host,
+        "protocol": protocol,
+        "first": first,
+        "last": last
+    }
+    
+    body = {
+        "kind": "biquery#tableDataInsertAllRequest",
+        "rows": [
+            {
+                "json": obj
+            }
+        ]
+    }
+
+    try:
+        result = tabledata.insertAll(projectId=project, datasetId=dataset,
+                                     tableId=table, body=body).\
+                                     execute(http)
+        if result.has_key("insertErrors"):
+            print json.dumps(obs)
+            print json.dumps(result)
+    except googleapiclient.errors.HttpError, e:
+        print "Table insert failed."
+        print e
+    except:
+        pass
 
 def curate():
     now = time.time()
@@ -37,8 +165,9 @@ def curate():
                 last = activity[k]["last"]
                 device = k[0]
                 host = k[1]
-                msg = "WEB %s: %s, %s - %s\n" % (device, host, first, last)
-                sys.stderr.write(msg)
+
+                insert_activity(device, host, "http", first, last)
+
                 del(activity[k])
                 changed=True
                 break
@@ -60,12 +189,17 @@ def update(device, host, tm):
 def handle(msg):
     if not msg["action"] == "http_request":
         return
-    
+
     try:
         update(msg["device"], msg["header"]["Host"], msg["time"])
     except:
-        host = None
-    
+        # e.g. missing HTTP headers?
+        pass
+
+############################################################################
+
+last_curate=time.time()
+
 while True:
     try:
         msg = skt.recv(flags=zmq.NOBLOCK)
