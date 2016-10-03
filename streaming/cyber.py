ce91eba1ce208474dba498550431acca92d543f1
Cyber MacGeddon <cybermaggedon@gmail.com>
2016-10-03 21:17:54 +0100

    Added HDFS loader into a Google Dataproc service

--- streaming/cyber.py
+++ streaming/cyber.py
@@ -11,26 +11,26 @@ context = wye.Context(url)
 job = context.define_job(name="cybermon",
                          description="Cybermon stream processor")
 
-dnsact = job.define_python_worker("dns-activity", "dns_activity.py")
-
-webact = job.define_python_worker("web-activity", "web_activity.py")
 
 trust = job.define_python_worker("trust-score", "trust_score.py")
 
+fp = job.define_python_worker("fingerprinter", "fingerprinter.py")
+fp.connect("output", [(trust, "input")])
+
+dnsact = job.define_python_worker("dns-activity", "dns_activity.py")
+webact = job.define_python_worker("web-activity", "web_activity.py")
 bq = job.define_python_worker("bigquery", "bigquery.py")
 es = job.define_python_worker("elasticsearch", "elasticsearch.py")
 gaffer = job.define_python_worker("gaffer", "gaffer.py")
 cassandra = job.define_python_worker("cassandra", "cassandra_store.py")
+hadoop = job.define_python_worker("hadoop", "hadoop.py")
 publisher = job.define_python_worker("publisher", "publisher.py")
-
-fp = job.define_python_worker("fingerprinter", "fingerprinter.py")
-fp.connect("output", [(trust, "input")])
 
 recv = job.define_python_worker("receiver", "receiver.py")
 recv.connect("output",
              [(webact, "input"), (dnsact, "input"), (fp, "input"),
               (bq, "input"), (es, "input"), (cassandra, "input"),
-              (publisher, "input")])
+              (hadoop, "input"), (publisher, "input")])
 
 job_id = job.implement()
 
---
+++ streaming/hadoop.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+
+import zmq
+import json
+import sys
+import requests
+import socket
+import os
+import time, datetime
+import hdfs
+import uuid
+
+############################################################################
+
+fqdn = socket.getfqdn()
+ctxt = zmq.Context()
+skt = ctxt.socket(zmq.PULL)
+port = skt.bind_to_random_port("tcp://*")
+
+input="tcp://%s:%d" % (fqdn, port)
+
+ctrl = os.fdopen(3, 'w')
+ctrl.write("INIT\n")
+ctrl.write("INPUT:input:%s\n" % input)
+ctrl.write("RUNNING\n")
+ctrl.flush()
+
+############################################################################
+    
+url = os.getenv("HDFS_URL", "http://hdfs:50070")
+user = "hdfs"
+basedir = "/cyberprobe/"
+
+############################################################################
+
+client = hdfs.client.InsecureClient(url, user=user)
+
+try:
+    client.makedirs(basedir)
+except Exception, e:
+    sys.stderr.write("Directory %s creation failed (ignored): %s\n" %
+                     (basedir, e))
+    sys.stderr.flush()
+
+data=[]
+
+count=0
+last=time.time()
+
+max_batch=64 * 1024 * 1024
+max_time=300
+
+def handle(msg):
+
+    global data, count, last
+
+    data.append(msg)
+
+    count = count + len(msg)
+
+    if (count > max_batch) or ((time.time() - last) > max_time):
+
+
+        u = str(uuid.uuid4())
+
+        dir = datetime.datetime.utcnow().strftime("%Y-%m-%d/%H-%M")
+        dir = basedir + dir
+
+        try:
+            client.makedirs(dir)
+        except Exception, e:
+            sys.stderr.write("Directory %s creation failed (ignored): %s\n" %
+                             (basedir, e))
+	    sys.stderr.flush()
+
+        filedata = "\n".join(data)
+        path = dir + "/" + u
+        
+        try:
+            with client.write(path, encoding='utf-8') as writer:
+                writer.write(filedata)
+            print "HDFS write %s." % path
+        except Exception, e:
+            sys.stderr.write("HDFS creation failed for %s (ignored): %s\n" %
+                             (path, e))
+	    sys.stderr.flush()
+
+        data = []
+        count = 0
+        last = time.time()
+
+############################################################################
+
+while True:
+    try:
+        msg = skt.recv()
+        handle(msg)
+    except Exception, e:
+        sys.stderr.write("hadoop: Exception: %s\n" % str(e))
+	sys.stderr.flush()
+        time.sleep(0.1)
+
